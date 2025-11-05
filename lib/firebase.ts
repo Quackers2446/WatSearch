33cc54450caf338313ba32e22fb1fba1eed7d814
Matthew Zhou <myzhou@uwaterloo.ca>
2025-11-04 19:19:52 -0500

    add firebase config

---
+++ lib/firebase.ts
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApps, FirebaseApp } from "firebase/app"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDahbA2cKzO4B-88077iuGDDlM1-lMYcCc",
+    authDomain: "watsearch-a8c9b.firebaseapp.com",
+    projectId: "watsearch-a8c9b",
+    storageBucket: "watsearch-a8c9b.firebasestorage.app",
+    messagingSenderId: "172319961836",
+    appId: "1:172319961836:web:39ebc021f8b9e5a0cfebc9",
+}
+
+// Initialize Firebase
+// Use singleton pattern to avoid multiple initializations
+let app: FirebaseApp
+
+if (getApps().length === 0) {
+    app = initializeApp(firebaseConfig)
+} else {
+    app = getApps()[0]
+}
+
+export default app
+
